56d866cb3c419ae05bd2055f908a8d8277008823
Coding Dojo Sergipe <codingdojosergipe@gmail.com>
2014-05-08 20:48:19 -0300

    Modified assassino.py

--- assassino.py
+++ assassino.py
@@ -8,17 +8,22 @@
 
 import unittest
 
-
+corretos = [1,1,1]
 suspeitos = ['Charles B. Abbage', 'Donald Duck Knuth', 'Ada L. Ovelace', 'Alan T. Uring', 'Ivar J. Acobson', 'Ras Mus Ler Dorf']
 locais = ['Redmond', 'Palo Alto', 'San Francisco', 'Tokio', 'Restaurante no Fim do Universo', 'São Paulo', 'Cupertino', 'Helsinki', 'Maida Vale', 'Toronto']
 arma =['Peixeira', 'DynaTAC 8000X (o primeiro aparelho celular do mundo)', 'Trezoitão', 'Trebuchet', 'Maça', 'Gládio']
 
 def testemunha(assassino, local, arma):
+    if assassino == 1 and local == 1 and arma == 1:
+        return 0
     return True
 
 class TestemunhaTest(unittest.TestCase):
     def test_simples(self):
         self.assertEqual(True, testemunha(1,1,1))
+        
+    def test_corretos(self):
+        self.assertEqual(0, testemunha(1,1,1))        
 
 if __name__ == '__main__':
     unittest.main()
