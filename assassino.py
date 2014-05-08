a05b30d86320503fe0934f5192c6c0604aaf6fdd
Coding Dojo Sergipe <codingdojosergipe@gmail.com>
2014-05-08 20:50:00 -0300

    Modified assassino.py

--- assassino.py
+++ assassino.py
@@ -8,7 +8,7 @@
 
 import unittest
 
-corretos = [1,1,1]
+corretos = [1,2,1]
 suspeitos = ['Charles B. Abbage', 'Donald Duck Knuth', 'Ada L. Ovelace', 'Alan T. Uring', 'Ivar J. Acobson', 'Ras Mus Ler Dorf']
 locais = ['Redmond', 'Palo Alto', 'San Francisco', 'Tokio', 'Restaurante no Fim do Universo', 'São Paulo', 'Cupertino', 'Helsinki', 'Maida Vale', 'Toronto']
 arma =['Peixeira', 'DynaTAC 8000X (o primeiro aparelho celular do mundo)', 'Trezoitão', 'Trebuchet', 'Maça', 'Gládio']
