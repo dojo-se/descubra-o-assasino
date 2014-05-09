cb26192f5b21dc49ce3f66b5604f726c5ffc5299
Coding Dojo Sergipe <codingdojosergipe@gmail.com>
2014-05-08 21:40:52 -0300

    Modified assassino.py

--- assassino.py
+++ assassino.py
@@ -35,7 +35,8 @@ def detetive():
                     random.choice(range(1,len(locais))),
                     random.choice(range(1,len(armas)))]
     while (True):
-        
+        if testemunha(chutes[0], chutes[1], chutes[2]):
+            
         
 
 class TestemunhaTest(unittest.TestCase):
