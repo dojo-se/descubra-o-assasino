736a9d13cfa6a5f8ef677ae0877481cbe35cc5b7
Coding Dojo Sergipe <codingdojosergipe@gmail.com>
2014-05-08 21:10:32 -0300

    Modified assassino.py

--- assassino.py
+++ assassino.py
@@ -52,6 +52,7 @@ class TestemunhaTest(unittest.TestCase):
         
     def test_info_testemunha_dois_errados(self):
         self.assertEqual([1,2], informacaoDaTestemunha(2,1,1))
+        self.assertEqual([2,3], informacaoDaTestemunha(1,1,2))
         
 
 if __name__ == '__main__':
